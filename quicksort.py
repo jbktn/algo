ba970bd35e1a90cd8d0437fce2e11d0a0aa3e5c1
Jakub Pomorski <80984903+jbktn@users.noreply.github.com>
2023-03-22 23:19:50 +0100

    Add files via upload

---
+++ quicksort.py
@@ -0,0 +1,22 @@
+def partition(array, low, high):
+    pivot = array[high]
+    i = low - 1
+    for j in range(low, high):
+        if array[j] <= pivot:
+            i = i + 1
+            (array[i], array[j]) = (array[j], array[i])
+    (array[i + 1], array[high]) = (array[high], array[i + 1])
+    return i + 1
+
+
+def quickSort(array, low, high):
+    if low < high:
+        pi = partition(array, low, high)
+        quickSort(array, low, pi - 1)
+        quickSort(array, pi + 1, high)
+
+
+n = int(input())
+arr = [int(input()) for i in range(n)]
+quickSort(arr, 0, len(arr) - 1)
+print(*arr)
